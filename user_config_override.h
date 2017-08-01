37d74103645ec944013fb8960091b5e1fcf6d867
TuHuynhthanh <tu.huynhthanh@stu.edu.vn>
2017-08-01 22:45:39 +0700

    upcode test

---
+++ user_config_override.h
@@ -0,0 +1,11 @@
+// Examples
+//#ifdef CFG_HOLDER
+//#undef CFG_HOLDER
+//#endif
+//#define CFG_HOLDER             0x20161210
+
+//#ifdef STA_SSID1
+//#undef STA_SSID1
+//#endif
+//#define STA_SSID1              "yourssid1"
+
